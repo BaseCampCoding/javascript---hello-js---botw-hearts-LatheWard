885be4ed387f2935910bc8f5fae0e595b91b6e1d
Lathe Ward <lward21@basecampcodingacademy.org>
2021-04-01 10:20:32 -0500

    Setting repo to git commit deee93a563b09

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const overhealButton = controlsContainer.querySelector("#overheal-button");
 const overhealAmountInput = controlsContainer.querySelector("#overheal-amount-input");
 
 let health = 35;
-let maxHealth = 44;
+let maxHealth = 40;
 let overhealAmount = 0;
 
 function randint(lo, hi) {
@@ -18,53 +18,36 @@ function randint(lo, hi) {
 }
 
 function updateHeartsDisplay() {
-  heartsContainer.innerHTML = "";
-  let healthRemaining = health
-  while (healthRemaining > 0){
-    const quarters = Math.min(healthRemaining, 4);
-    heartWidget = document.createElement('div');
-    heartsContainer.insertAdjacentHTML(
-    "beforeend",
-    `<div class="heart" data-quarters="${quarters}">
-          <div class="top-left"></div>
-          <div class="top-right"></div>
-          <div class="bottom-left"></div>
-          <div class="bottom-right"></div>
-        </div>`
-      );
-      healthRemaining -= 4;
+  let quartersToFill = health;
+  for (const heart of heartsContainer.querySelectorAll(".heart")) {
+    if (quartersToFill) {
+      let quarters = Math.min(quartersToFill, 4);
+      heart.dataset.quarters = quarters;
+      quartersToFill -= quarters;
+    } else {
+      heart.dataset.quarters = 0;
+    }
   }
-  let emptyHearts = Math.floor((maxHealth - health) / 4)
-  for (let i = 0; i < emptyHearts; i++){
-
-  }
-
 }
-  
 
 hitButton.addEventListener("click", function () {
   let damage = Number(hitDamageInput.value);
   health = Math.max(0, health - damage);
   updateHeartsDisplay();
 
-  if (newExtraHeart.dataset.quarters == 0){
-    heartsContainer.removeChild(newExtraHeart)}
+  heartsContainer.removeChild(newExtraHeart)
 });
 
 healButton.addEventListener("click", function (){
-  let heal = Number(healAmountInput.value) * 4;
+  let heal = Number(healAmountInput.value);
   health = Math.max(0, health + heal);
   updateHeartsDisplay();
 });
 
 ahcButton.addEventListener("click", function (){
-  maxHealth += 4;
-  health = maxHealth
-  updateHeartsDisplay();
-
   const newHeart = heartsContainer.querySelector(".heart").cloneNode(true);
   heartsContainer.appendChild(newHeart);
-  
+  maxHealth += 4;
   if (health !== maxHealth){
     health += (maxHealth - health);
   };
@@ -75,16 +58,15 @@ const newExtraHeart = document.querySelector(".heart").cloneNode(true)
 newExtraHeart.className = "heart extra"
 
 overhealButton.addEventListener("click", function(){
-  // if (newExtraHeart.hasChildNodes == false ){
+  if (newExtraHeart.hasChildNodes == false ){
   for (let i = overhealAmountInput.value - overhealAmount / 4; i!= 0; i--){
     heartsContainer.appendChild(newExtraHeart);
-    maxHealth += 4;
   };
-  
+  maxHealth += 4;
   if (health !== maxHealth){
     health += (maxHealth - health);
   };
-  updateHeartsDisplay();  
+  updateHeartsDisplay();  }
 
 })
 
